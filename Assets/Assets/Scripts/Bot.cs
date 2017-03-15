4ed695c62bae1caa1e016b3fadc3c8478ec4625e
Steve Losh <steve@stevelosh.com>
2017-03-15 11:06:24 +0000

    Add bots

---
+++ Assets/Assets/Scripts/Bot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour {
+	[SerializeField] GameObject[] models;
+	[SerializeField] GameObject model;
+
+	void Start () {
+		ChooseModel();
+	}
+
+	private void ChooseModel() {
+		GameObject modelPrefab = models[Random.Range(0, models.Length)];
+		model = Instantiate(modelPrefab, this.transform.position, Quaternion.identity);
+		model.transform.parent = this.transform;
+	}
+}
---
+++ Assets/Assets/Scripts/BotSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BotSpawner : MonoBehaviour {
+	[SerializeField] GameObject botPrefab;
+	[SerializeField] int botCount;
+
+	void Start () {
+		for (int i = 0; i < botCount; i++) {
+			SpawnBot();
+		}
+	}
+
+	private void SpawnBot() {
+		Transform spawnPoint = NetworkManager.singleton.GetStartPosition();
+		Instantiate(botPrefab, spawnPoint.position, Quaternion.identity);
+	}
+}
